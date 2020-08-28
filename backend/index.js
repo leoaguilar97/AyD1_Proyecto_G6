3202a7ef4e68fab294c1b60c6fefeac6aa0a0f23
Leo <leoaguilarsanchez97@gmail.com>
2020-08-28 11:08:31 -0600

    Agregado index.js para inicio de API

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,17 @@ const app = express();
 const port = 5000;
 
 app.get('/', (req, res) => {
+<<<<<<< Updated upstream
   return res.send('API V2 ERPPLUS WORKING');
+=======
+  return res.send('API ERPPLUS WORKING');
+>>>>>>> Stashed changes
 });
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`API iniciada, puerto ${port}`);
+<<<<<<< Updated upstream
 });
+=======
+});
+>>>>>>> Stashed changes
