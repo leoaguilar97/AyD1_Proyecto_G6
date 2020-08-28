e7d1f0832444333e4a0ad3abcd94d8b0b7d0ab14
Leo <leoaguilarsanchez97@gmail.com>
2020-08-28 11:15:40 -0600

    Agregada conexion a MongoAtlas, y rutas de prueba

---
+++ backend/app/models/index.js
@@ -0,0 +1,13 @@
+const dbConfig = require("../config/db.config.js");
+
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+
+const db = {};
+db.mongoose = mongoose;
+db.url = dbConfig.url;
+db.tutorials = require("./tutorial.model.js")(mongoose);
+db.usuario = require("./usuario.js")(mongoose);
+db.producto = require("./producto")(mongoose);
+
+module.exports = db;
