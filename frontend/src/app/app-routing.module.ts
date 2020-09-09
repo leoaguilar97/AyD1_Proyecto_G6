809cb3c1e14af60997f34cccf463bc3813125168
David Salguero <mada.salgui@gmail.com>
2020-09-09 17:06:40 -0600

    Agregados los componentes para sprint 3

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -24,6 +24,9 @@ import { HomeComponent } from './home/home.component';
 import { EditarUsuarioComponent } from './editarUsuario/editar-usuario.component';
 import { LoginComponent } from './login/login.component';
 import { EditarProductoComponent } from './editar-producto/editar-producto.component';
+import { SedesComponent } from './sedes/sedes.component';
+import { CategoriasComponent } from './categorias/categorias.component';
+import { ProveedoresComponent } from './proveedores/proveedores.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -50,7 +53,10 @@ const routes: Routes = [
   { path: 'editarUsuario/:dpi_usuario', component: EditarUsuarioComponent },
   { path: 'editarProducto/:id', component: EditarProductoComponent },
   { path: 'editarProducto/:id', component: EditarProductoComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'sedes', component: SedesComponent },
+  { path: 'categorias', component: CategoriasComponent },
+  { path: 'proveedores', component: ProveedoresComponent }
 ];
 
 @NgModule({
