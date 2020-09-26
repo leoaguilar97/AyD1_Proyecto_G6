6d51b5bd44b3baedd92f3310caf754cb7442051b
Francisco Lezana <fraaanciscolezana8@gmail.com>
2020-09-25 22:37:04 -0600

    Se implementó prueba de TDD para obtener categorías

--- frontend/src/app/categorias/categorias.component.spec.ts
+++ frontend/src/app/categorias/categorias.component.spec.ts
@@ -1,16 +1,36 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CategoriasComponent } from './categorias.component';
+import { fakeAsync, tick, async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { Router, Routes } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Location } from "@angular/common";
+import { NgZone } from '@angular/core';
+import { EditarCategoriasComponent } from '../editar-categorias/editar-categorias.component';
+
 
 describe('CategoriasComponent', () => {
   let component: CategoriasComponent;
   let fixture: ComponentFixture<CategoriasComponent>;
+  let location: Location;
+  let router: Router;
+
+  const routes: Routes = [
+    { path: 'editarProducto/:id', component: EditarCategoriasComponent }
+  ];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CategoriasComponent ]
+      imports: [FormsModule, RouterTestingModule.withRoutes(routes), HttpClientModule],
+      declarations: [CategoriasComponent, EditarCategoriasComponent]
     })
-    .compileComponents();
+      .compileComponents();
+
+    router = TestBed.get(Router);
+    location = TestBed.get(Location);
+    fixture = TestBed.createComponent(CategoriasComponent);
+    router.initialNavigation();
   }));
 
   beforeEach(() => {
@@ -19,7 +39,10 @@ describe('CategoriasComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  //Test 1
+  it('Debe retornar True al cargar los Categorías', async(() => {
+    expect(component.cargarCategs()).toBeTrue();
+  }));
+
+  
 });
--- frontend/src/app/editar-categorias/editar-categorias.component.spec.ts
+++ frontend/src/app/editar-categorias/editar-categorias.component.spec.ts
@@ -18,8 +18,4 @@ describe('EditarCategoriasComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
