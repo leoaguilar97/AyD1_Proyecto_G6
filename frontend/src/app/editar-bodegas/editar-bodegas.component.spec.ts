626f7218bb859bffd507428a3c22ed5f5230548f
Francisco Lezana <fraaanciscolezana8@gmail.com>
2020-09-26 21:35:02 -0600

    Setup pruebas TDD sedes.component bodega.component

--- frontend/src/app/editar-bodegas/editar-bodegas.component.spec.ts
+++ frontend/src/app/editar-bodegas/editar-bodegas.component.spec.ts
@@ -18,8 +18,4 @@ describe('EditarBodegasComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
