5b5e0bb4b1210f0ac1d406f18bd6b2aa60671cef
David Salguero <mada.salgui@gmail.com>
2020-10-21 01:53:15 -0600

    Consumido el servicio http GET para obtener productos en la vista de venta

--- frontend/src/app/venta/venta.component.ts
+++ frontend/src/app/venta/venta.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-venta',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VentaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private http: HttpClient) { }
+  bodegas = [];
+  personas = [];
+  nombre_cliente: String;
+  direccion_cliente: String;
+  nit_cliente: String;
+  vendedor: String;
+  bodega: String;
 
   ngOnInit(): void {
+    this.cargarBodegas();
+    this.cargarVendedores();
   }
-
+  cargarBodegas() {
+    this.http.get('https://api-erpp.herokuapp.com/api/bodega')
+      .toPromise().then((data: any) => {
+        this.bodegas = data.bodegas;
+      });
+  }
+  cargarVendedores() {
+    this.http.get('https://api-erpp.herokuapp.com/api/usuario')
+      .toPromise().then((data: any) => {
+        this.personas = data;
+      });
+  }
+  finalizar() {
+    console.log(this.nombre_cliente);
+    console.log(this.direccion_cliente);
+    console.log(this.nit_cliente);
+    console.log(this.vendedor);
+    console.log(this.bodega);
+  }
 }
