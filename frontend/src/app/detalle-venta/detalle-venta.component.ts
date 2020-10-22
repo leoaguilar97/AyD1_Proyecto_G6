452acbd32b72a1cc580f26d4589163adddba6586
Francisco Lezana <fraaanciscolezana8@gmail.com>
2020-10-22 01:07:08 -0600

    Consumido el servicio http GET para obtener ventas

--- frontend/src/app/detalle-venta/detalle-venta.component.ts
+++ frontend/src/app/detalle-venta/detalle-venta.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-detalle-venta',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetalleVentaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private http: HttpClient) { }
 
+  //
+  ventas = [];
   ngOnInit() {
+    this.cargarVentas();
+  }
+
+  cargarVentas(): boolean {
+    this.http.get('https://api-erpp.herokuapp.com/api/venta')
+      .toPromise().then((data: any) => {
+        this.ventas = data.ventas;
+      });
+    return true;
   }
 
 }
