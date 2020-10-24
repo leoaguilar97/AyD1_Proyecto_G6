64cf7532fc88007962d3a25a159495b384adf4d3
David Salguero <mada.salgui@gmail.com>
2020-10-23 23:06:38 -0600

    Consumido servicio http POST para reportar una venta

--- frontend/src/app/venta/venta.component.ts
+++ frontend/src/app/venta/venta.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { Venta, Producto, Factura } from '../models/modelos';
 
 @Component({
   selector: 'app-venta',
@@ -14,6 +15,7 @@ export class VentaComponent implements OnInit {
   personas = [];
   productos = [];
   ventas = [];
+  producto_facturados = [];
   nombre_cliente: String;
   direccion_cliente: String;
   nit_cliente: String;
@@ -24,7 +26,9 @@ export class VentaComponent implements OnInit {
   cantidad_producto: String;
   disponibles_producto: String;
   precio_producto: String;
+  nombre_producto: String;
   bodegaSeleccionada = false;
+  total = 0;
 
   ngOnInit(): void {
     this.cargarBodegas();
@@ -43,7 +47,38 @@ export class VentaComponent implements OnInit {
       });
   }
   finalizar() {
-    console.log(this.productos);
+    const factura: Factura = {
+      nombre_cliente: this.nombre_cliente,
+      nit: this.nit_cliente,
+      direccion: this.direccion_cliente,
+      vendedor: this.vendedor,
+      bodega: this.bodega,
+      productos: this.producto_facturados
+    };
+    this.http.post('https://api-erpp.herokuapp.com/api/venta', factura)
+    .toPromise().then((data: any) => {
+      this.limpiarDatos();
+      });
+  }
+  limpiarDatos() {
+  this.bodegas = [];
+  this.personas = [];
+  this.productos = [];
+  this.ventas = [];
+  this.producto_facturados = [];
+  this.nombre_cliente = '';
+  this.direccion_cliente = '';
+  this.nit_cliente = '';
+  this.vendedor = '';
+  this.bodega = '';
+  this.id_producto_auxiliar = '';
+  this.id_producto = '';
+  this.cantidad_producto = '';
+  this.disponibles_producto = '';
+  this.precio_producto = '';
+  this.nombre_producto = '';
+  this.bodegaSeleccionada = false;
+  this.total = 0;
   }
   seleccionBodega() {
     console.log(this.bodega);
@@ -60,12 +95,31 @@ export class VentaComponent implements OnInit {
         this.disponibles_producto = producto.cantidad;
         this.precio_producto = producto.precio;
         this.id_producto = producto.producto._id;
+        this.nombre_producto = producto.producto.nombre;
       }
     });
   }
   agregar() {
-    console.log(this.precio_producto);
-    console.log(this.cantidad_producto);
-    console.log(this.id_producto);
+    const producto: Producto = {
+      cantidad: this.cantidad_producto,
+      producto: this.id_producto
+    };
+    this.producto_facturados.push(producto);
+
+    const venta: Venta = {
+      id: this.id_producto,
+      nombre: this.nombre_producto,
+      cantidad: this.getNumber(this.cantidad_producto),
+      precio: this.getNumber(this.precio_producto)
+    };
+    this.ventas.push(venta);
+    const total = this.getTotal(this.cantidad_producto, this.precio_producto);
+    this.total += total;
+  }
+  getNumber(cantidad: String): Number {
+    return Number(cantidad);
+  }
+  getTotal(cantidad: String, precio: String): number {
+    return Number(cantidad) * Number(precio);
   }
 }
