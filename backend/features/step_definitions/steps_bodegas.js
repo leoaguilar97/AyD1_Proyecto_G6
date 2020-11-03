397aee9d9d8852771f3cfeba7d6646b95bc43a7f
Leo <leoaguilarsanchez97@gmail.com>
2020-11-03 08:01:00 -0600

    Arreglo

--- backend/features/step_definitions/steps_bodegas.js
+++ backend/features/step_definitions/steps_bodegas.js
@@ -229,7 +229,7 @@ Then('se espera un error {int} ya que la bodega no existe', function(code) {
 });
 
 Given('una bodega que se le quieren inserter ciertos productos', async function() {
-    throw "Error";
+    //throw "Error";
     post.json = {
         "bodega": bodega.id,
         "productos": [{
